b6895697fdf273bd47b842f5e0222256af9d7a8e
Luis Balbás <lbq200@gmail.com>
2022-05-17 16:13:44 -0400

    Added form method and action, fixed component name

--- app/pages/addComment.js
+++ app/pages/addComment.js
@@ -5,14 +5,14 @@ import Button from './components/Button.js'
 import Link from 'next/Link';
 import styles from './addCamground.module.scss';
 
-export default function navbar() {
+export default function addComment() {
 	return (
 		<div className={styles.addComment}>
 		<Navbar />
-		<form className={styles.commentForm}>
+		<form action={process.env.NEXT_PUBLIC_API_URI + "/addComment"} method="post" className={styles.commentForm}>
 			<h1 className="bold">Add New Comment</h1>
 			<control>
-					<label for="description">Description</label>
+					<label htmlFor="description">Description</label>
 					<textarea rows="8" cols="80" name="description" placeholder="This was probably the best camp I've visited this past year, definitely recomend visiting any time soon." type="textbox"/>
 			</control>
 			<Button>Add Coment</Button>
