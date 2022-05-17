90f357aa87b0b057226afa8c60b75c7ee8258c0a
Luis Balbás <lbq200@gmail.com>
2022-05-17 16:13:18 -0400

    Fixed for attribute

--- app/pages/addCampground.js
+++ app/pages/addCampground.js
@@ -13,20 +13,20 @@ export default function addCampground() {
 			<form action={process.env.NEXT_PUBLIC_API_URI + "/addCampground"} method="post" className={styles.campgroundForm}>
 				<h1 className="bold">Add New Campground</h1>
 				<control>
-						<label for="name">Campground Name</label>
+						<label htmlFor="name">Campground Name</label>
 						<input name="name" placeholder="Seven Sisters Waterfall" type="text"/>
 				</control>
 				<control>
-						<label for="price">Price</label>
+						<label htmlFor="price">Price</label>
 						<input name="price" placeholder="$149" type="text"/>
 				</control>
 				<control>
-						<label for="name">Image</label>
+						<label htmlFor="name">Image</label>
 						<input name="image" placeholder="www.thepinoytraveler.com/2018/01/mt-ulap-diy-dayhike.html" 
 				type="text"/>
 				</control>
 				<control>
-						<label for="description">Description</label>
+						<label htmlFor="description">Description</label>
 						<textarea rows="5" cols="80" name="description" placeholder="Seven Sisters Waterfall" type="textbox"/>
 				</control>
 				<input className={styles.submit} placeholder="Add Campground" type="submit"/>
