a4c1ab246ff2b79684f8ea9bcd764bdb0adb90ea
Luis Balbás <lbq200@gmail.com>
2022-08-23 16:56:36 -0400

    Update addCampground.js

--- app/pages/addCampground.js
+++ app/pages/addCampground.js
@@ -1,35 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar.js'
 import Footer from './components/Footer.js'
 import Button from './components/Button.js'
 import Link from 'next/Link';
 import styles from './addCamground.module.scss';
+import axios from 'axios';
 
 export default function addCampground() {
+	const [campName, setName] = useState("");
+	const [campPrice, setPrice] = useState("");
+	const [campImage, setImage] = useState("");
+	const [campDesc, setDesc] = useState("");
+
+	function sendForm(e){
+		e.preventDefault();
+		axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+		axios.post(process.env.NEXT_PUBLIC_API_URI + "/addCampground", {
+	    name: campName,
+	    price: campPrice,
+	    image: campImage,
+	    desc: campDesc,
+	  })
+	  .then(function (response) {
+	    console.log(response);
+	  })
+	  .catch(function (error) {
+	    console.log(error);
+	  });
+	}
+
 	return (
 		<div>
 			<Navbar />
-			<form action={process.env.NEXT_PUBLIC_API_URI + "/addCampground"} method="post" className={styles.campgroundForm}>
+			<form method="post" className={styles.campgroundForm}>
 				<h1 className="bold">Add New Campground</h1>
 				<control>
 						<label htmlFor="name">Campground Name</label>
-						<input required name="name" placeholder="Seven Sisters Waterfall" type="text"/>
+						<input onKeyUp={(e)=> setName(e.target.value)} required name="name" placeholder="Seven Sisters Waterfall" type="text"/>
 				</control>
 				<control>
 						<label htmlFor="price">Price</label>
-						<input required name="price" placeholder="$149" type="text"/>
+						<input onKeyUp={(e)=> setPrice(e.target.value)} required name="price" placeholder="$149" type="text"/>
 				</control>
 				<control>
-						<label htmlFor="name">Image</label>
-						<input required name="image" placeholder="www.thepinoytraveler.com/2018/01/mt-ulap-diy-dayhike.html" 
+						<label htmlFor="image">Image</label>
+						<input onKeyUp={(e)=> setImage(e.target.value)} required name="image" placeholder="www.thepinoytraveler.com/2018/01/mt-ulap-diy-dayhike.html" 
 				type="text"/>
 				</control>
 				<control>
 						<label htmlFor="description">Description</label>
-						<textarea required rows="5" cols="80" name="description" placeholder="Seven Sisters Waterfall" type="textbox"/>
+						<textarea onKeyUp={(e)=> setDesc(e.target.value)} required rows="5" cols="80" name="description" placeholder="Seven Sisters Waterfall" type="textbox"/>
 				</control>
-				<input className={styles.submit} placeholder="Add Campground" type="submit"/>
-			</form>
+				<a onClick={e => sendForm(e)} type="button"  className={styles.submit}>Submit</a>
+				</form>
 			<Footer/>
 		</div>
 	)
