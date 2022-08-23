55fd5317e01314eafeb309fe9e11b7663aed43ff
Luis Balbás <lbq200@gmail.com>
2022-08-23 16:56:25 -0400

    Added cors

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import { MongoClient, ServerApiVersion } from 'mongodb';
 import {config} from 'dotenv'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 config();
 const app = express()
 const db = process.env.DB_URI;
@@ -10,19 +11,23 @@ const client = new MongoClient(db, { useNewUrlParser: true, useUnifiedTopology: 
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors())
+app.options('*', cors())
+
 
 app.post('/addCampground',(req,res)=>{
-  client.connect(async (err) => {
-    const collection = client.db("yelpCamp").collection("campgrounds");
-    let camp = {
+  let camp = {
       name : req.body.name,
       price: req.body.price,
       image: req.body.image,
-      desc: req.body.description,
-    };
+      desc: req.body.desc,
+  };
+  client.connect(async (err) => {
+    const collection = client.db("yelpCamp").collection("campgrounds");
     const result = await collection.insertOne(camp);
     client.close();
-  }); 
+    res.status(201).send()
+  }) 
 })
 
 app.listen(3001)
